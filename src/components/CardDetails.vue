8714e9d8c6cb45e76cf8df334510ca2957cf7854
Ahmad Allehyany <aallehyany@gmail.com>
2020-11-08 23:42:14 +0300

    view details from store

--- src/components/CardDetails.vue
+++ src/components/CardDetails.vue
@@ -1,5 +1,35 @@
 <template>
-    <div class="w-full h-full absolute inset-0 bg-black opacity-25 z-50">
-        <div class="container mx-auto"></div>
+    <div class="w-full h-full absolute inset-0 bg-black opacity-95 z-50 items-center flex overflow-hidden" @click="hideDetails">
+        <div class="max-w-screen-md mx-auto flex justify-center">
+            <img :src="card.image_url" alt="" class="card object-contain">
+            <div class="div flex flex-col ml-1"> 
+                <h1 class="w-full text-white">{{card.name}}</h1>
+                <p class="text-white">{{card.text}}</p>
+            </div>
+        </div>
     </div>
 </template>
+<script>
+import { computed } from 'vue';
+import { useStore } from 'vuex'
+export default {
+
+    setup() {
+        const store=  useStore();
+        const card = computed(() => store.state.previewCard);
+        const hideDetails = () => {
+            store.commit('setPreviewCard', null)
+        }
+        return {
+            card,
+            hideDetails
+        }
+    }
+}
+</script>
+<style scoped>
+.card {
+    width:200px;
+    height: 279px;
+}
+</style>
