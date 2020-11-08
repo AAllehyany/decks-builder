5e630ee769704deaa83b6dc400ea63eaa13d9cd4
Ahmad Allehyany <aallehyany@gmail.com>
2020-11-08 23:41:56 +0300

    minor fix

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
-  <div id="app">
+<div id="app">
     <header class="p-2 flex shadow-md mb-3">
-      <div class="w-full flex mx-auto justify-between items-center">
+    <div class="w-full flex mx-auto justify-between items-center">
         <div>
-          <h1 class="text-gray-800 text-base uppercase font-bold">deck builder</h1>
+        <h1 class="text-gray-800 text-base uppercase font-bold">deck builder</h1>
         </div>
         <div>
-          <input type="search" 
+        <input type="search" 
             class=" w-40 p-1 bg-transparent text-gray-800 focus:outline-none hover:outline-none border-solid border-b border-b-1 border-gray-800" 
             placeholder="enter deck code..." />
         </div>
-      </div>
+    </div>
     </header>
     <router-view />
-  </div>
+</div>
 </template>
