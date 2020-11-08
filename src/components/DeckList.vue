a56234cce63c2540e98dbca752914cc46bd01572
Ahmad Allehyany <aallehyany@gmail.com>
2020-11-08 20:11:03 +0300

    Component to view deck list

---
+++ src/components/DeckList.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="w-full bg-gray-300 overflow-y-auto deck-list rounded-md border-4 border-teal-800">
+        <p v-for="(card, i) in deck" :key="i" class="truncate bg-teal-800 p-1 w-full text-white text-base mb-px">{{card.name}}</p>
+    </div>
+</template>
+<script>
+export default {
+    props: {
+        deck: Array
+    }
+}
+</script>
+<style scoped>
+@layer utilities {
+    @responsive {
+        .deck-list {
+            height: 32rem;
+        }
+    }
+}
+</style>
