2b7dba63a36013082a1dcf35913576347fee12ef
Ahmad Allehyany <aallehyany@gmail.com>
2020-11-09 03:23:00 +0300

    modal fixed

--- src/components/CardDetails.vue
+++ src/components/CardDetails.vue
@@ -1,9 +1,13 @@
 <template>
-    <div class="w-full h-full absolute inset-0 bg-black opacity-95 z-50 items-center flex overflow-hidden" @click="hideDetails">
-        <div class="max-w-screen-md mx-auto flex justify-center">
-            <img :src="card.image_url" alt="" class="card object-contain">
-            <div class="div flex flex-col ml-1"> 
-                <h1 class="w-full text-white">{{card.name}}</h1>
+    <div class="w-full h-full fixed left-0 bottom-0 bg-black opacity-95 z-50 items-center flex overflow-hidden">
+        <div class="max-w-screen-md mx-auto flex flex-col lg:flex-row justify-center">
+            <img :src="card.image_url" alt="" class="card object-contain"  @click="hideDetails">
+            <div class="flex flex-col ml-1 p-3"> 
+                <h1 class="w-full text-white font-bold">{{card.name}}</h1>
+                <div class="flex mb-3">
+                    <span class="text-sm text-white mr-3"><b>Traits: </b> {{card.traits}}</span>
+                    <span class="text-sm text-white mr-3"><b>Level: </b> {{card.level}}</span>
+                </div>
                 <p class="text-white">{{card.text}}</p>
             </div>
         </div>
