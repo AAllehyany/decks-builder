07f1254c9695eac88a5704fd61b079bb6a22ae22
Ahmad Allehyany <aallehyany@gmail.com>
2020-11-11 15:45:39 +0300

    vertical

--- src/components/CardDetails.vue
+++ src/components/CardDetails.vue
@@ -1,10 +1,10 @@
 <template>
-    <div class="w-full h-full fixed left-0 bottom-0 bg-black opacity-95 z-50 items-center flex overflow-hidden">
-        <div class="max-w-screen-md mx-auto flex flex-col lg:flex-row justify-center">
-            <img :src="card.image_url" alt="" class="card object-contain"  @click="hideDetails">
-            <div class="flex flex-col ml-1 p-3"> 
-                <h1 class="w-full text-white font-bold">{{card.name}}</h1>
-                <div class="flex mb-3">
+    <div class="w-full h-full fixed left-0 top-0 bg-black opacity-95 z-50 flex overflow-x-hidden overflow-y-auto">
+        <div class="max-w-md mx-auto flex flex-col justify-center">
+            <img :src="card.image_url" alt="" class="w-1/2 mx-auto"  @click="hideDetails">
+            <div class="flex flex-col ml-1 p-3 w-full"> 
+                <h1 class="w-full text-white font-bold text-center">{{card.name}}</h1>
+                <div class="flex mb-3 justify-center">
                     <span class="text-sm text-white mr-3"><b>Traits: </b> {{card.traits}}</span>
                     <span class="text-sm text-white mr-3"><b>Level: </b> {{card.level}}</span>
                 </div>
@@ -22,6 +22,7 @@ export default {
         const store=  useStore();
         const card = computed(() => store.state.previewCard);
         const hideDetails = () => {
+            document.body.classList.remove('modal-open')
             store.commit('setPreviewCard', null)
         }
         return {
